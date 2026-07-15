266e5f344ca9b37cfd499aff0837da5da06ee9de
mostakschl-eng <mostakschl@gmail.com>
2026-07-15 13:04:57 +0600

    add verel

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
-import adapter from "@sveltejs/adapter-node";
+import nodeAdapter from "@sveltejs/adapter-node";
+import vercelAdapter from "@sveltejs/adapter-vercel";
+
+const adapter = process.env.VERCEL === "1" ? vercelAdapter() : nodeAdapter();
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter,
   },
 };
 
